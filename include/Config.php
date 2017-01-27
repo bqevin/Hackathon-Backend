ff9e87a24540d6ae8bd15c8b9e272bf9445e4d9a
Kevin Barasa <kevin.barasa001@gmail.com>
2017-01-27 03:29:25 +0300

    Added Users code

---
+++ include/Config.php
@@ -0,0 +1,10 @@
+<?php
+ 
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "amn_arosto");
+define("DB_PASSWORD", "Kevcom2015");
+define("DB_DATABASE", "amn_arosto");
+?>
